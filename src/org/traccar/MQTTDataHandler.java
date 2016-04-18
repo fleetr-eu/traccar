981a0ae76e3e405b1966b842628d250e385e118d
Gantcho Kojuharov <gantcho@greyarch.net>
2016-04-18 23:48:08 +0200

    uncommented mqtt publishing code; correctly replacing fixTime attribute in the mqtt template

--- src/org/traccar/MQTTDataHandler.java
+++ src/org/traccar/MQTTDataHandler.java
@@ -32,7 +32,7 @@ import org.traccar.model.Position;
 
 public class MQTTDataHandler extends BaseDataHandler {
 
-	
+
 	private static int qos = 2;
 	private static String topic = null;
 	private static MqttClient client = null;
@@ -45,22 +45,23 @@ public class MQTTDataHandler extends BaseDataHandler {
 	public MQTTDataHandler() {
 		initMQTTClient();
 	}
-	
+
 	private String formatRequest(Position position) {
-		
+
 		Device device = Context.getIdentityManager().getDeviceById(position.getDeviceId());
-		
+
 		short state = updateState(position, device);
-		
+
 		String template = Context.getConfig().getString("mqtt.template");
 
 		String request = template
 			.replace("##name##", device.getName())
-			.replace("##uniqueId##", device.getUniqueId())			
-			.replace("##deviceId##", String.valueOf(position.getDeviceId()))	
+			.replace("##uniqueId##", device.getUniqueId())
+			.replace("##deviceId##", String.valueOf(position.getDeviceId()))
 			.replace("##protocol##", String.valueOf(position.getProtocol()))
 			.replace("##eventType##", String.valueOf(position.getAttributes().get("eventType")))
 			.replace("##deviceTime##", String.valueOf(position.getDeviceTime().getTime()))
+			.replace("##fixTime##", String.valueOf(position.getFixTime().getTime()))
 			.replace("##valid##", String.valueOf(position.getValid()))
 			.replace("##latitude##", String.valueOf(position.getLatitude()))
 			.replace("##longitude##", String.valueOf(position.getLongitude()))
@@ -71,27 +72,27 @@ public class MQTTDataHandler extends BaseDataHandler {
 			.replace("##io##", String.valueOf(position.getAttributes().get("io")))
 			.replace("##idle##", String.valueOf(position.getAttributes().get("idle")))
 			.replace("##address##", position.getAddress() != null ?  position.getAddress() : "")
-		 	.replace("##attributes##", MiscFormatter.toJsonString(position.getAttributes()));	 
+		 	.replace("##attributes##", MiscFormatter.toJsonString(position.getAttributes()));
 
 		return request;
 	}
 
 	private short updateState(Position position, Device device) {
-		
+
 		Short newPowerState = (Short) position.getAttributes().get(Event.KEY_POWER);
 		Short previousPowerState = power.get(device.getUniqueId());
-		
+
 		String trip = trips.get(device.getUniqueId()) == null ? UUID.randomUUID().toString() : trips.get(device.getUniqueId());
 		String rest = rests.get(device.getUniqueId()) == null ? UUID.randomUUID().toString() : rests.get(device.getUniqueId());
-		
+
 		short state = 1;
 		short io = 255;
 		int eventType = 30;
-		if (previousPowerState != newPowerState) {			
+		if (previousPowerState != newPowerState) {
 			power.put(device.getUniqueId(), newPowerState);
 			eventType = 29;
-			
-			if (newPowerState == 1) { // new trip			
+
+			if (newPowerState == 1) { // new trip
 				trip = UUID.randomUUID().toString();
 				state = 1;
 				io = 255;
@@ -100,22 +101,22 @@ public class MQTTDataHandler extends BaseDataHandler {
 				state = 0;
 				io = 254;
 			}
-		} 
+		}
 		position.set("eventType", eventType);
 		position.set("io", io);
-		
+
 		trips.put(device.getUniqueId(), trip);
 		rests.put(device.getUniqueId(), rest);
-		
+
 		position.set("trip", trip);
 		position.set("rest", rest);
-		
-		if (position.getSpeed() < minIdleSpeed) { 
+
+		if (position.getSpeed() < minIdleSpeed) {
 			String idle = idles.get(device.getUniqueId());
 			if (idle == null) { // new idle
 				idle = UUID.randomUUID().toString();
 				idles.put(device.getUniqueId(), idle);
-			} 
+			}
 			position.set("idle", idle);
 			if (state == 1) {
 				position.set("idle", true);
@@ -123,19 +124,19 @@ public class MQTTDataHandler extends BaseDataHandler {
 		} else {
 			idles.remove(device.getUniqueId());
 		}
-		
+
 		return state;
 	}
-	
+
 	protected static MqttClient initMQTTClient() {
 		if (client != null) {
 			return client;
-		} 
+		}
 		String url = Context.getConfig().getString("mqtt.url");
 		String clientId = Context.getConfig().getString("mqtt.clientId");
-		String user = Context.getConfig().getString("mqtt.user"); 
+		String user = Context.getConfig().getString("mqtt.user");
 		String password = Context.getConfig().getString("mqtt.password");
-		topic = Context.getConfig().getString("mqtt.topic"); 
+		topic = Context.getConfig().getString("mqtt.topic");
 		qos = Context.getConfig().getInteger("mqtt.qos");
 		minIdleSpeed = Double.parseDouble(Context.getConfig().getString("fleetr.minIdleSpeed"));
 		try {
@@ -152,21 +153,21 @@ public class MQTTDataHandler extends BaseDataHandler {
 		}
 		return client;
 	}
-	
+
 	@Override
 	protected Position handlePosition(Position position) {
-		
+
 		String content = formatRequest(position);
-		
+
 		System.out.println("Publishing to topic="+topic+": "+content);
-			
+
 		MqttMessage message = new MqttMessage(content.getBytes());
 		message.setQos(qos);
-//		try {
-//			client.publish(topic, message);
-//		} catch (MqttException e) {
-//			e.printStackTrace();
-//		}
+		try {
+			client.publish(topic, message);
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
 		return position;
 	}
 
